e818a27cef2c37f935637af0c9fac50d066f4cd9
rishab-beaconstac <rishabh.j@beaconstac.com>
2023-06-05 22:11:04 +0530

    added image upload api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -88,9 +88,18 @@ class RecipeSerializer(serializers.ModelSerializer):
         return instance
 
 
-
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipe."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True' }}
